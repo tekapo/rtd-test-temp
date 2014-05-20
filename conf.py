bda444b0e57e14e89dcdf820fc1f6ac48832304b
JOTAKI Taisuke <tekapo@gmail.com>
2014-05-20 15:52:35 +0900

    update

--- conf.py
+++ conf.py
@@ -31,6 +31,7 @@ from datetime import datetime
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+sys.path.insert(0, os.path.abspath('.'))
 # extensions = []
 extensions = ['sphinx.ext.autodoc', 'releases']
 
