66275e525a196923ebb9721c70106e169c018e8f
JOTAKI Taisuke <tekapo@gmail.com>
2014-05-20 15:50:22 +0900

    update

--- conf.py
+++ conf.py
@@ -12,8 +12,11 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
+from __future__ import with_statement
 import os
+import sys
+import types
+from datetime import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
